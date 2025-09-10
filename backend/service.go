e2a1957163720acb15f83b42d64c8c921458f8d2
Arrobo, Gabriel <gabriel.arrobo@intel.com>
2025-09-10 09:17:19 -0700

    Replace unmantained despencendy git.cs.nctu.edu.tw/calee/sctp with github.com/ishidawataru/sctp
    Signed-off-by: Arrobo, Gabriel <gabriel.arrobo@intel.com>

--- backend/service.go
+++ backend/service.go
@@ -12,20 +12,14 @@ import (
 	"sync"
 	"syscall"
 
-	"git.cs.nctu.edu.tw/calee/sctp"
+	"github.com/ishidawataru/sctp"
 	"github.com/omec-project/ngap"
 	"github.com/omec-project/sctplb/logger"
 )
 
-type SCTPHandler struct {
-	HandleMessage      func(conn net.Conn, msg []byte)
-	HandleNotification func(conn net.Conn, notification sctp.Notification)
-}
+type SCTPHandler func(conn net.Conn, msg []byte)
 
 const readBufSize uint32 = 8192
-
-// set default read timeout to 2 seconds
-var readTimeout syscall.Timeval = syscall.Timeval{Sec: 2, Usec: 0}
 
 var (
 	sctpListener *sctp.SCTPListener
@@ -33,16 +27,12 @@ var (
 )
 
 var sctpConfig sctp.SocketConfig = sctp.SocketConfig{
-	InitMsg:   sctp.InitMsg{NumOstreams: 3, MaxInstreams: 5, MaxAttempts: 2, MaxInitTimeout: 2},
-	RtoInfo:   &sctp.RtoInfo{SrtoAssocID: 0, SrtoInitial: 500, SrtoMax: 1500, StroMin: 100},
-	AssocInfo: &sctp.AssocInfo{AsocMaxRxt: 4},
+	InitMsg: sctp.InitMsg{NumOstreams: 3, MaxInstreams: 5, MaxAttempts: 2, MaxInitTimeout: 2},
 }
 
 func ServiceRun(addresses []string, port int) {
 	logger.AppLog.Infoln("service Run is called")
-	handler := SCTPHandler{
-		HandleMessage: dispatchMessage,
-	}
+	handler := SCTPHandler(dispatchMessage)
 
 	ips := []net.IPAddr{}
 
@@ -64,14 +54,14 @@ func ServiceRun(addresses []string, port int) {
 }
 
 func listenAndServe(addr *sctp.SCTPAddr, handler SCTPHandler) {
-	if listener, err := sctpConfig.Listen("sctp", addr); err != nil {
+	listener, err := sctpConfig.Listen("sctp", addr)
+	if err != nil {
 		logger.SctpLog.Errorf("failed to listen: %+v", err)
 		return
-	} else {
-		sctpListener = listener
 	}
+	sctpListener = listener
 
-	logger.SctpLog.Infoln("listen on", sctpListener.Addr())
+	logger.SctpLog.Infof("listen on %s", sctpListener.Addr())
 
 	for {
 		newConn, err := sctpListener.AcceptSCTP()
@@ -129,16 +119,6 @@ func listenAndServe(addr *sctp.SCTPAddr, handler SCTPHandler) {
 			logger.SctpLog.Debugf("set read buffer to %d bytes", readBufSize)
 		}
 
-		if err := newConn.SetReadTimeout(readTimeout); err != nil {
-			logger.SctpLog.Errorf("set read timeout error: %+v, accept failed", err)
-			if err = newConn.Close(); err != nil {
-				logger.SctpLog.Errorf("close error: %+v", err)
-			}
-			continue
-		} else {
-			logger.SctpLog.Debugf("set read timeout: %+v", readTimeout)
-		}
-
 		logger.SctpLog.Infof("[AMF] SCTP Accept from: %s", newConn.RemoteAddr().String())
 		peer := &SctpConnections{}
 		peer.conn = newConn
@@ -164,7 +144,7 @@ func handleConnection(conn *sctp.SCTPConn, bufsize uint32, handler SCTPHandler) 
 		for {
 			buf := make([]byte, bufsize)
 
-			n, info, notification, err := conn.SCTPRead(buf)
+			n, info, err := conn.SCTPRead(buf)
 			if err != nil {
 				switch err {
 				case io.EOF, io.ErrUnexpectedEOF:
@@ -184,33 +164,22 @@ func handleConnection(conn *sctp.SCTPConn, bufsize uint32, handler SCTPHandler) 
 				}
 			}
 
-			if notification != nil {
-				p, ok := connections.Load(conn)
-				if !ok {
-					logger.SctpLog.Warnln("notification for unknown connection")
-				} else {
-					peer := p.(*SctpConnections)
-					logger.SctpLog.Infof("handle SCTP Notification peer %v ", peer.address)
-					GnbConnChan <- false
-				}
-			} else {
-				if info == nil || info.PPID != ngap.PPID {
-					logger.SctpLog.Warnln("received SCTP PPID != 60, discard this packet")
-					continue
-				}
-
-				logger.SctpLog.Debugf("read %d bytes", n)
-				logger.SctpLog.Debugf("packet content: %+v", hex.Dump(buf[:n]))
-
-				handler.HandleMessage(conn, buf[:n])
+			if info == nil || info.PPID != ngap.PPID {
+				logger.SctpLog.Warnln("received SCTP PPID != 60, discard this packet")
+				continue
 			}
+
+			logger.SctpLog.Debugf("read %d bytes", n)
+			logger.SctpLog.Debugf("packet content: %+v", hex.Dump(buf[:n]))
+
+			handler(conn, buf[:n])
 		}
 	}()
 
 	for x := range GnbConnChan {
 		logger.SctpLog.Infoln("closing gnb Connection:", x)
 		buf := make([]byte, bufsize)
-		handler.HandleMessage(conn, buf[:0])
+		handler(conn, buf[:0])
 		return
 	}
 }
